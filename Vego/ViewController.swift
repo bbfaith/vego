27aef34fc3d527345e334e2ed2b9f5885b3528ff
Faith Ben <Ben@faiths-macbook-pro.local>
2016-05-20 22:59:17 +1000

    Initial Commit

---
+++ Vego/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vego
+//
+//  Created by Faith Ben on 20/05/2016.
+//  Copyright © 2016 Yanhong Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
